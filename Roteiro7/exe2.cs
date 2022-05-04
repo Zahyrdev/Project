9b06b75492d4a2cb70afea7735be0f0c0ca7b2b9
Zahyrdev <102634224+Zahyrdev@users.noreply.github.com>
2022-05-04 20:08:48 -0300

    rot7

---
+++ Roteiro7/exe2.cs
@@ -0,0 +1,18 @@
+﻿    using System;
+
+    namespace roteiro7
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int i, n, S=0;
+                Console.WriteLine("Informe N: ");
+                n = int.Parse(Console.ReadLine());
+                for(i=0; i<n; i++){
+                    S=S+i;
+                    Console.WriteLine("S= "+ S);
+                }
+            }
+        }
+    }
